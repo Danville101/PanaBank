8482d2ca988e14d550e3d515d509a95ea2df74ac
Danville101 <danville.wilks@gmail.com>
2024-02-20 16:56:42 +0000

    Added K8 files Need to split DB names in backend for smiple k8 transition

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -40,3 +40,41 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceControlle
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks_iam_role.name
 }
+
+
+resource "aws_eks_node_group" "panad_bank_node_group" {
+  cluster_name =  aws_eks_cluster.panda_bank_eks.name
+
+node_role_arn       = aws_iam_role.example.arn
+  subnet_ids          = module.vpc.subnet_ids 
+  ami_type            = "AL2_x86_64"
+  instance_types      = "t3.mico"
+  capacity_type       = "SPOT"
+  disk_size           = 20
+
+  tags = {
+    Environment = "production"
+    Team        = "engineering"
+  }
+
+  remote_access {
+    ec2_ssh_key = "ec2_key_pair"
+  }
+
+  scaling_config {
+    desired_size = 3
+    min_size     = 1
+    max_size     = 5
+  }
+
+  launch_template {
+    id = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  taint {
+    key    = "dedicated"
+    value  = "special"
+    effect = "NO_SCHEDULE"
+}
+}
